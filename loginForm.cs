fe729593911a884710c1c1060e8dddec9256b360
Gözde <142896767+gozdetanyildiz@users.noreply.github.com>
2024-05-19 14:10:20 +0300

    Personel Takip Sistemi
    c# Forms ile geliştirilmiştir.

---
+++ loginForm.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace vtys
+{
+    public partial class loginForm : Form
+    {
+        SqlConnection con;
+        SqlDataReader dr;
+        SqlCommand com;
+
+        public loginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGiris_Click(object sender, EventArgs e)
+        {
+            string selectedOption = cmbyetki.SelectedItem.ToString();
+            string user = kullanici_adi.Text;
+            string password = sifre.Text;
+
+            con = new SqlConnection("Data Source=DESKTOP-8D4PG33\\SQLEXPRESS;Initial Catalog=Ornek;Integrated Security=True"); //Data Source=DESKTOP-8D4PG33\\SQLEXPRESS;Initial Catalog=Ornek;Integrated Security=True"
+            com = new SqlCommand(); 
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "Select*From Kullanici_Bilgi where kullanici_adi='"+user+"'And sifre='"+password+"'";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                MessageBox.Show("Tebrikler Giriş Başarılı");
+                if (selectedOption == "Müşteri Temsilcisi")
+                {
+                    musteritemForm form1 = new musteritemForm(); // Yetki 1'e göre formu oluştur
+                    form1.Show();
+                }
+                else if (selectedOption == "Takım Lideri")
+                {
+                    takimlidForm form2 = new takimlidForm(); // Yetki 2'ye göre formu oluştur
+                    form2.Show();
+                }
+                else if (selectedOption == "Grup Yöneticisi")
+                {
+                    grupyoneticisiform form3 = new grupyoneticisiform(); // Yetki 3'e göre formu oluştur
+                    form3.Show();
+                }
+            }
+            else
+            { 
+                MessageBox.Show("Hatalı Kullanıcı Adı veya Şifre");
+            }
+            con.Close();
+
+        }
+
+        private void loginForm_Load(object sender, EventArgs e)
+        {
+            con = new SqlConnection("Data Source=DESKTOP-8D4PG33\\SQLEXPRESS;Initial Catalog=Ornek;Integrated Security=True");
+            SqlCommand com = new SqlCommand("SELECT * FROM Yetkiler",con);
+            SqlDataReader dr;
+            con.Open();
+            dr = com.ExecuteReader();
+
+            while (dr.Read())
+            {
+                cmbyetki.Items.Add(dr["yetkiler"]);
+                
+            }
+            con.Close();
+            if (con.State != ConnectionState.Closed) //con kapanıp kapanmadığını kontrol eden kod
+            {
+                con.Close();
+            }
+
+
+        }
+    }
+}
---
+++ mustericagrilistesiFormcs.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace vtys
+{
+    public partial class mustericagrilistesiForm : Form
+    {
+        // Veritabanı bağlantı dizesi
+        private readonly string connectionString = "Data Source=DESKTOP-8D4PG33\\SQLEXPRESS;Initial Catalog=Ornek;Integrated Security=True";
+
+        public mustericagrilistesiForm()
+        {
+            InitializeComponent();
+            InitializeDataGridView();
+            GetList();
+        }
+
+        private void InitializeDataGridView()
+        {
+            // DataGridView sütunlarını tanımla
+            DataGridView1.Columns.Add("MusAdSoyad", "Müşteri Adı Soyadı");
+            DataGridView1.Columns.Add("GorusmeBaslama", "Görüşme Başlama Saati");
+            DataGridView1.Columns.Add("GorusmeBitis", "Görüşme Bitiş Saati");
+            DataGridView1.Columns.Add("Gorusmekonusu", "Görüşme Konusu");
+            DataGridView1.Columns.Add("GorusmeDurumu", "Görüşme Durumu");
+            DataGridView1.Columns.Add("GorusmeTarihi", "Görüşme Tarihi");
+        }
+
+        private void GetList()
+        {
+            try
+            {
+                // Veritabanından verileri getir
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter("SELECT * FROM Musteri_Bilgileri", connection);
+                    DataTable dataTable = new DataTable();
+                    dataAdapter.Fill(dataTable);
+
+                    // DataGridView'e verileri yükle
+                    foreach (DataRow row in dataTable.Rows)
+                    {
+                        DataGridView1.Rows.Add(
+                            row["MusAdSoyad"].ToString(),
+                            row["GorusmeBaslama"].ToString(),
+                            row["GorusmeBitis"].ToString(),
+                            row["Gorusmekonusu"].ToString(),
+                            row["GorusmeDurumu"].ToString(),
+                            row["GorusmeTarihi"].ToString()
+                        );
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Veritabanı bağlantı dizesi
+                string connectionString = "Data Source=DESKTOP-8D4PG33\\SQLEXPRESS;Initial Catalog=Ornek;Integrated Security=True";
+
+                // Veritabanındaki değişiklikleri kaydet
+                SqlDataAdapter dataAdapter = new SqlDataAdapter("SELECT * FROM Musteri_Bilgi", connectionString);
+                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+                DataGridView1.EndEdit();
+                dataAdapter.Update(dataTable);
+
+                MessageBox.Show("Veriler başarıyla güncellendi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void yenicagribtn_Click(object sender, EventArgs e)
+        {
+            // Yeni çağrı formunu aç
+            yenicagriForm yenicagriForm = new yenicagriForm();
+            yenicagriForm.FormClosed += (s, args) => {
+                // Form kapatıldığında DataGridView'i yenile
+                DataGridView1.Rows.Clear();
+                GetList();
+            };
+            this.Hide();
+            yenicagriForm.ShowDialog();
+            
+        }
+    }
+}
+ 
